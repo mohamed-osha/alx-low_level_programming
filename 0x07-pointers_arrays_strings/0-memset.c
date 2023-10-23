ae9146fe72e7f0e1aa4fc4a444a4295644cfa29e
mohamed-osha <mohamed0yassergr@gmail.com>
2023-10-23 14:29:49 -0700

    “commit”

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+root@b592a8f9b713:~/alx-low_level_programming/0x07-pointers_arrays_strings# cat 0-memset.c
+#include "main.h"
+/**
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
